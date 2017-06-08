fdec3db78427e1b33f26eee547ed32a3521bdbb0
Chen <xinchenv@isoftstone.com>
2017-06-08 11:06:08 +0800

    add cors support add public folder and index.html

--- Node/BasicServer/app.js
+++ Node/BasicServer/app.js
@@ -5,8 +5,11 @@ let host = "localhost";
 let express = require('express');
 let http  = require('http');
 let app = express();
+let cors = require('cors');
 //use ./public folder as static file folder
 app.use(express.static(__dirname+"/public"));
+app.options('*', cors());
+app.use(cors());
 
 var server = http.createServer(app).listen(port, function() {
 	console.log("server start on http://localhost:"+port);
--- Node/README.md
+++ Node/README.md
@@ -1,2 +1,15 @@
-# CodeSample 
+# NodeJS相关的各种例子 
 ## BasicServer
+
+最简单的服务器，使用express插件，包含一个静态文件目录，监听8100端口。使用cors插件支持基本的跨域访问请求。
+## Post
+
+对各种post进行处理，获取post的参数
+
+## Upload
+
+使用multer插件实现了post上传的功能。上传的文件会保存在uploads目录。
+
+## Rest
+
+Rest 服务器的实现
