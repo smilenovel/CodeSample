b8dfcfca1d3564ec8fa9ec6526a7950219adadb7
Chen <xinchenv@isoftstone.com>
2017-06-08 11:40:01 +0800

    complete app.js

--- Node/Post/app.js
+++ Node/Post/app.js
@@ -6,11 +6,16 @@ let express = require('express');
 let http  = require('http');
 let app = express();
 let bodyParser = require('body-parser');
-let multer = require('multer');
-let upload = multer();
 //use ./public folder as static file folder
 app.use(express.static(__dirname+"/public"));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.post("/post", function(req, res, next){
+	//post parameters is req.body
+	res.send(req.body);
+});
 var server = http.createServer(app).listen(port, function() {
 	console.log("server start on http://localhost:"+port);
 });
